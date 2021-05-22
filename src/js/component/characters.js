53942cdc21050ca400c2e0c769b2365a8a6c330a
tebanban <estebancalf@gmail.com>
2021-05-22 14:40:31 +0000

    title styles!

--- src/js/component/characters.js
+++ src/js/component/characters.js
@@ -15,12 +15,12 @@ const Characters = () => {
 
 	return (
 		<div className="container pb-4">
-			<h3>Characters</h3>
+			<h3 className="blogSubtitle">Characters</h3>
 			<span className="altfavList">{JSON.stringify(store.favorites)}</span>
 			<div className="d-flex justify-content-between overflow-auto">
 				{store.peopleList.map((item, index) => {
 					return (
-						<Card className="mx-2 p-1" key={Math.random() * 1000} style={{ minWidth: "16rem" }}>
+						<Card className="mx-2 p-1 card" key={Math.random() * 1000} style={{ minWidth: "16rem" }}>
 							<img src={cardImg} />
 							<Link to={"/single/" + index}>
 								<h5>{item.name}</h5>
